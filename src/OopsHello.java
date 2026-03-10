9b5104b6c5e4d2cdc4a44935d8aa956fac4383a8
Tosha <toshanidutta@gmail.com>
2026-03-10 14:04:50 +0530

    added UC2

--- src/OopsHello.java
+++ src/OopsHello.java
@@ -1,5 +1,12 @@
-public class OopsHello{
+public class OopsHello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
+
     }
 }
