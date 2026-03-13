102ff2666a4a0a63cd4b5835d58b259dff40ce30
Tosha <toshanidutta@gmail.com>
2026-03-13 15:10:09 +0530

    added UC4

--- src/OopsHello.java
+++ src/OopsHello.java
@@ -2,14 +2,26 @@ public class OopsHello {
 
     public static void main(String[] args) {
 
-        String name;
+        String names;
 
         if (args.length > 0) {
-            name = args[0];
+
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < args.length; i++) {
+                builder.append(args[i]);
+
+                if (i < args.length - 1) {
+                    builder.append(", ");
+                }
+            }
+
+            names = builder.toString();
+
         } else {
-            name = "World";
+            names = "World";
         }
 
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Hello, " + names + "!");
     }
 }
